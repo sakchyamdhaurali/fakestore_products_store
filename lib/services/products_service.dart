4e159172ae4a91527b45cc6d95c166bf6b57eea1
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-01 19:07:28 +0545

    added product service

---
+++ lib/services/products_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:fakestore_products_store_app/models/products_model.dart';
+import 'package:http/http.dart' as http;
+
+class ProductsService {
+  final String _baseUrl = "https://fakestoreapi.com/products";
+
+  Future<List<Products>> getProducts() async {
+    final response = await http.get(Uri.parse(_baseUrl));
+    if (response.statusCode == 200) {
+      List<Products> productsList = [];
+      final List<dynamic> data = jsonDecode(response.body);
+      for (Map<String, dynamic> i in data) {
+        productsList.add(Products.fromJson(i));
+      }
+      return productsList;
+    } else {
+      throw Exception("Failed to load products");
+    }
+  }
+}
