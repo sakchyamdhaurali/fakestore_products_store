6ac729eaf7833952531e65e72b49c1b4fb76157b
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-01 19:13:32 +0545

    required setup for grid view

--- lib/widgets/product_list.dart
+++ lib/widgets/product_list.dart
@@ -1,16 +1,24 @@
+import 'package:fakestore_products_store_app/models/products_model.dart';
 import 'package:flutter/material.dart';
 
 class ProductList extends StatelessWidget {
-const ProductList({ Key? key }) : super(key: key);
+  final List<Products> products;
+  const ProductList({super.key, required this.products});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        ),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 2/3
+      ),
+      itemCount: products.length,
       itemBuilder: (context, index) {
-        
+
       },
     );
   }
