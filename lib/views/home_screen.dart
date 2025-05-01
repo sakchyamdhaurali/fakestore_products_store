34110311e39a5c0b9257beecc6b62a0525715561
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-01 21:24:22 +0545

    safearea removed

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -22,102 +22,100 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Container(
      color: const Color(0xFFCECECE),
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-          appBar: AppBar(
-            title: const Text("FakeStore API"),
-            backgroundColor: const Color(0xFFCECECE),
-          centerTitle: true,
-          ),
-          body: Column(
-            children: [
-              Expanded(
-                child: FutureBuilder<List<Products>>(
-                  future: fetchAPI,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                      return const Center(child: Text('No products found.'));
-                    } else {
-                      final products = snapshot.data!;
-                      return GridView.builder(
-                        padding: const EdgeInsets.all(10),
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
-                          childAspectRatio: 2 / 3,
-                        ),
-                        itemCount: products.length,
-                        itemBuilder: (context, index) {
-                          final product = products[index];
-                          return Card(
-                            color: Colors.white,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Image.network(product.image!, fit: BoxFit.cover),
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        appBar: AppBar(
+          title: const Text("FakeStore API"),
+          backgroundColor: const Color(0xFFCECECE),
+        centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder<List<Products>>(
+                future: fetchAPI,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No products found.'));
+                  } else {
+                    final products = snapshot.data!;
+                    return GridView.builder(
+                      padding: const EdgeInsets.all(10),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 2 / 3,
+                      ),
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        final product = products[index];
+                        return Card(
+                          color: Colors.white,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Image.network(product.image!, fit: BoxFit.cover),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    product.title!,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
+                                ),
+      
+                                Row(children: [
+      
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      product.title!,
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-
-                                  Row(children: [
-
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text("PRICE: ",
-                                      style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w600,
-                                        ),),
-                                    ),
-
-                                     Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      product.price.toString(),
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
+                                    child: Text("PRICE: ",
+                                    style: TextStyle(
                                         fontSize: 14,
                                         fontWeight: FontWeight.w600,
-                                      ),
+                                      ),),
+                                  ),
+      
+                                   Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    product.price.toString(),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
                                     ),
                                   ),
-
-                                  ],),
-                                  
-                                ],
-                              ),
+                                ),
+      
+                                ],),
+                                
+                              ],
                             ),
-                          );
-                        },
-                      );
-                    }
-                  },
-                ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
